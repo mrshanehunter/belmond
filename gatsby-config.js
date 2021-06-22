e7baf47ceaf6f9ea81b0e9b224d2ffc0e0ab27bc
shunts <67136434+mrshanehunter@users.noreply.github.com>
2021-06-22 18:19:09 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteMetadata: {
+    title: "Belmond",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "belmond",
+        dataset: "production",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "GA-1234",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
